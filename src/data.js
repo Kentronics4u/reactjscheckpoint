c4cd56d214d7ff10dc7b86cbbe1fd02814d83af4
Kentronics <kentronics4u@gmail.com>
2023-11-13 15:31:11 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,17 @@
+export const cardItems = [
+  {
+    img: "https://images.unsplash.com/photo-1695653422699-de49031f060e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxzZWFyY2h8MXx8dGVjaG5vbG9neXxlbnwwfHwwfHx8MA%3D%3D",
+    cardTitle: "Greater heights",
+    cardText: "Our great days are here.",
+  },
+  {
+    img: "https://plus.unsplash.com/premium_photo-1682145373962-4d76b19410ab?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    cardTitle: "The way to go",
+    cardText: "Engineering...making life easier.",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1519389950473-47ba0277781c?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    cardTitle: "Networking",
+    cardText: "Like minds.",
+  },
+];
